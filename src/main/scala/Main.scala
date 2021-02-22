1ea15c9b524e0ddcbafac108526327f0f39a74b8
scumethy <scume7hy@gmail.com>
2021-02-22 21:04:22 +0300

    completed logic

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,9 +7,10 @@ import scalafx.geometry.Orientation
 import scalafx.geometry.Pos.Center
 import scalafx.scene.Scene
 import scalafx.scene.control.Alert.AlertType
-import scalafx.scene.control.{Alert, Button, TableColumn, TableView, TextField}
+import scalafx.scene.control.{Alert, Button, Label, TableColumn, TableView, TextField}
 import scalafx.scene.input.MouseEvent
 import scalafx.scene.layout.{BorderPane, FlowPane, HBox, VBox}
+import scalafx.scene.shape.Line
 
 class RelationMatrix(numOfRows: Int, numOfCols: Int) {
     private var _relationMatrix = Array.tabulate(numOfRows, numOfCols)((x, y) ⇒ 0)
@@ -38,7 +39,7 @@ object Main extends JFXApp {
     stage = new PrimaryStage {
         title = "Relationship Visualize"
 
-        val root = new BorderPane() {
+        val root = new HBox(spacing = 40) {
             id = "back-pane"
             val user_box = new FlowPane(Orientation.Vertical) {
                 this.vgap = 20
@@ -48,7 +49,7 @@ object Main extends JFXApp {
                 val setB = new TextField() {
                     id = "set-B-field"
                 }
-                val expression = new TextField() {
+                val expressionField = new TextField() {
                     id = "expression-field"
                     text = "x + y"
                 }
@@ -57,9 +58,17 @@ object Main extends JFXApp {
                     text = "Вывести матрицу"
                 }
                 printMatrixButton.onMouseClicked = (me: MouseEvent) ⇒ {
-                    val relationMatrix = createRelationMatrix(setA, setB, expression)
+                    val setAVals :: setBVals :: Nil = (setA :: setB :: Nil).map(tf ⇒ {
+                        extractInputFromTextField(tf)
+                            .split(" ")
+                            .map(_.toInt)
+                    })
+                    val expression = extractInputFromTextField(expressionField).split(" ")
 
-                    relationMatrix.getRelationMatrixArray.foreach(r ⇒ {
+                    val relationMatrix = createRelationMatrix(setAVals, setBVals, expression)
+                    val relationMatrixArray = relationMatrix.getRelationMatrixArray
+
+                    relationMatrixArray.foreach(r ⇒ {
                         val relationRow = new HBox()
                         r.foreach(el ⇒ {
                             relationRow.children.add(
@@ -73,16 +82,49 @@ object Main extends JFXApp {
                         println(relationRow.children)
                         relMatrixPane.children.add(relationRow)
                     })
-                    println(relMatrixPane.children)
+
+                    // relation graph number columns
+                    val setAValsColumn = new FlowPane(Orientation.Vertical) {
+                        setAVals.foreach(v ⇒ {
+                            children.add(
+                                new Label(v.toString)
+                            )
+                        })
+                    }
+                    val relationsPane = new BorderPane()
+                    val setBValsColumn = new FlowPane(Orientation.Vertical) {
+                        setBVals.foreach(v ⇒ {
+                            children.add(
+                                new Label(v.toString)
+                            )
+                        })
+                    }
+                    relGraphBox.children.addAll(setAValsColumn, relationsPane, setBValsColumn)
+
+                    // relation graph lines between numbers
+                    val (a_x, b_x) = (-40, 40)
+                    var (a_y, b_y) = (14, 14)
+                    for (i ← setAVals.indices; a = setAValsColumn.children(i)) {
+                        for (j ← setBVals.indices; b = setBValsColumn.children(j)) {
+                            if (relationMatrixArray(i)(j) == 1) {
+                                relationsPane.children.addOne(Line(a_x, a_y, b_x, b_y))
+                            }
+                            b_y += 30
+                        }
+                        b_y = 14
+                        a_y += 30
+                    }
                 }
-                children.addAll(setA, setB, expression, printMatrixButton)
+                children.addAll(setA, setB, expressionField, printMatrixButton)
             }
-            var relMatrixPane = new FlowPane(Orientation.Vertical) {
+            val relMatrixPane = new FlowPane(Orientation.Vertical) {
                 this.setId("relMatrixPane")
             }
+            val relGraphBox = new HBox(spacing = 50) {
+                this.setId("relGraphBox")
+            }
 
-            left = user_box
-            center = relMatrixPane
+            children.addAll(user_box, relMatrixPane, relGraphBox)
         }
 
         scene = new Scene(root, 1000, 500) {
@@ -90,20 +132,14 @@ object Main extends JFXApp {
         }
     }
 
-    def createRelationMatrix(setATF: TextField, setBTF: TextField, expressionTF: TextField): RelationMatrix = {
-        val setAVals :: setBVals :: Nil = (setATF :: setBTF :: Nil).map(tf ⇒ {
-            extractInputFromTextField(tf)
-                .split(" ")
-                .map(_.toInt)
-        })
-        val expression = extractInputFromTextField(expressionTF).split(" ")
+    def createRelationMatrix(setA: Array[Int], setB: Array[Int], expression: Array[String]): RelationMatrix = {
         val operation = expression(1)
         val isCompareExp = expression.length == 5
         val compareNumber = if (isCompareExp) expression(4).toInt
 
-        var relMatrix = RelationMatrix.initMatrix(setAVals.length, setBVals.length)
-        for (i ← setAVals.indices; a = setAVals(i)) {
-            for (j ← setBVals.indices; b = setBVals(j)) {
+        var relMatrix = RelationMatrix.initMatrix(setA.length, setB.length)
+        for (i ← setA.indices; a = setA(i)) {
+            for (j ← setB.indices; b = setB(j)) {
                 val isRelation = operation match {
                     case "+" ⇒ a + b == compareNumber
                     case "-" ⇒ a - b == compareNumber
